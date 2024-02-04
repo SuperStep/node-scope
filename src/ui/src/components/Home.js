5e5a5489192d61869cfda23091f6a480ab55f8e4
Gordeev Egor <superstep007@gmail.com>
2024-02-04 13:33:58 +0300

    Made mui cards form, fixed deps

--- src/ui/src/components/Home.js
+++ src/ui/src/components/Home.js
@@ -1,41 +1,73 @@
 import React, {useLayoutEffect, useState} from "react";
+import {Card, CardContent, Typography} from "@mui/material";
 
-export function Home () {
 
-    const getMetrics = async() => {
+
+
+export function Home() {
+
+    const [data, setData] = useState();
+
+    const getMetrics = async () => {
         const res = await fetch("/api/v1/info");
         const metrics = await res.json();
-        setMetrics(metrics);
+        setData(metrics);
     }
-
-    const [metrics, setMetrics] = useState();
 
     useLayoutEffect(() => {
-       getMetrics().catch(e => console.log(e));
+        getMetrics().catch(e => console.log(e));
     });
 
-    if(metrics) {
-
+    if (data) {
         return (
-            <>
-                <label>Operating System</label>
-                <input type="text" value={metrics.operatingSystem} readOnly />
-                <br />
-                <label>Uptime</label>
-                <input type="text" value={`${metrics.uptime.days} days, ${metrics.uptime.hours} hours, ${metrics.uptime.minutes} minutes, ${metrics.uptime.seconds} seconds`} readOnly />
-                <br />
-                <label>RAM Info</label>
-                <input type="text" value={metrics.ramInfo} readOnly />
-                <br />
-                <label>Processor Info</label>
-                <input type="text" value={`Cores: ${metrics.processorInfo.cores}, Name: ${metrics.processorInfo.name}, Max Frequency: ${metrics.processorInfo.maxFreq} Hz, Usage: ${metrics.processorInfo.usage}%`} readOnly />
-                <br />
-                <label>Storage Info</label>
-                <input type="text" value={`Total Space: ${metrics.storageInfo.totalSpace} bytes, Free Space: ${metrics.storageInfo.freeSpace} bytes, Usage: ${metrics.storageInfo.usage}`} readOnly />
-            </>
+            <div style={{ display: 'flex', flexDirection: 'column', gap: 20 }}>
+                <Card style={{ backgroundColor: '#2196f3', color: '#fff' }}>
+                    <CardContent>
+                        <Typography variant="h5" gutterBottom>
+                            Operating System
+                        </Typography>
+                        <Typography variant="subtitle1">{data.operatingSystem}</Typography>
+                    </CardContent>
+                </Card>
+
+                <Card style={{ backgroundColor: '#2196f3', color: '#fff' }}>
+                    <CardContent>
+                        <Typography variant="h5" gutterBottom>
+                            Uptime
+                        </Typography>
+                        <Typography variant="subtitle1">{`${data.uptime.days} days, ${data.uptime.hours} hours, ${data.uptime.minutes} minutes`}</Typography>
+                    </CardContent>
+                </Card>
+
+                <Card style={{ backgroundColor: '#2196f3', color: '#fff' }}>
+                    <CardContent>
+                        <Typography variant="h5" gutterBottom>
+                            RAM Info
+                        </Typography>
+                        <Typography variant="subtitle1">{data.ramInfo}</Typography>
+                    </CardContent>
+                </Card>
+
+                <Card style={{ backgroundColor: '#2196f3', color: '#fff' }}>
+                    <CardContent>
+                        <Typography variant="h5" gutterBottom>
+                            Processor Info
+                        </Typography>
+                        <Typography variant="subtitle1">{`${data.processorInfo.name} (Cores: ${data.processorInfo.cores}, Max Frequency: ${data.processorInfo.maxFreq} Hz)`}</Typography>
+                    </CardContent>
+                </Card>
+
+                <Card style={{ backgroundColor: '#2196f3', color: '#fff' }}>
+                    <CardContent>
+                        <Typography variant="h5" gutterBottom>
+                            Storage Info
+                        </Typography>
+                        <Typography variant="subtitle1">{`Total Space - ${data.storageInfo.totalSpace} bytes, Free Space - ${data.storageInfo.freeSpace} bytes, Usage - ${data.storageInfo.usage}`}</Typography>
+                    </CardContent>
+                </Card>
+            </div>
         )
-    }
-    else {
+    } else {
         return (<p>Loading</p>)
     }
 }
